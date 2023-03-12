491970acf0d9cb59318c806142f9baa6df7b8afc
stekat <stefandietsch@gmail.com>
2023-03-12 05:53:17 +0100

    f

--- svelte.config.js
+++ svelte.config.js
@@ -16,8 +16,8 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter({
 			// fallback: 'index.html',
-			pages: 'build',
-			assets: 'build',
+			// pages: 'build',
+			// assets: 'build',
 		}
 		),
 		paths: {
